dab28f9435b9170f749624cfec23d91c205b649b
Arman Ali <toplinegeeks@gmail.com>
2025-10-20 16:15:39 +0500

    global api  working check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [PrismaModule,AuthModule]
+  imports: [  ConfigModule.forRoot({
+      isGlobal: true,
+    }),PrismaModule,AuthModule],
+   controllers: [AppController],
 })
 export class AppModule {}
